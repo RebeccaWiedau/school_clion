492d9e77de26ac23b21b08be4424f9311b7b8a17
Rebecca Wiedau <rebecca.wiedau@nrwsoft.de>
2021-03-31 10:30:29 +0200

    first

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include "tools/output.h"
 int main()
   {
-//    int array[] = { 10, 19, 7, 3, 5, 6, 9};
-//    tools::array_output(array, 7, "Array without sorting");
-//    quicksort(array, 7);
-//    tools::array_output(array, 7);
+    int array[] = { 10, 19, 7, 3, 5, 6, 9};
+    tools::array_output(array, 7, "Array without sorting");
+    quicksort(array, 7, 0, 6);
+    tools::array_output(array, 7);
     int one[] = {10, 19, 7, 3, 5, 6, 9};
     tools::array_output(one, 7, "Array without sorting");
     quicksort(one, 7, 0, 6);
@@ -21,5 +21,11 @@ int main()
   tools::array_output(three, 7, "Array without sorting");
   quicksort(three, 7, 0, 6);
   tools::array_output(three, 7, "Array with sorting");
+    std::cout << std::endl;
+  int four[] = {134, 19, 3, 329, 21, 456, 21, 0, 22, 1, 4, 9, 10, 18};
+  int four_length = std::size(four);
+  tools::array_output(four, four_length, "Array without sorting");
+  quicksort(four, four_length, 0, four_length - 1);
+  tools::array_output(four, 14, "Array with sorting");
     return 0;
   }
--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -1,13 +1,11 @@
 #include "quicksort.h"
-#include <iostream>
 #include "../tools/output.h"
+#include <iostream>
 
+void quicksort(int array[], int length, int index_left, int index_right, bool kill_switch)
+{
 
-
-
-void quicksort(int array[], int length, int index_left, int index_right) {
-
-  if (index_left + 1 == index_right || index_right <= 0) return;
+  if (index_left >= index_right) return;
   int pivot_index = index_right;
   int pivot = array[pivot_index];
   bool pivot_swaped = false;
@@ -19,93 +17,41 @@ void quicksort(int array[], int length, int index_left, int index_right) {
         if (j < i) {
           array[pivot_index] = array[i];
           array[i] = pivot;
-          tools::array_output(array, 7, "End");
+//          tools::array_output(array, 14, "End");
           pivot_swaped = true;
           pivot_index = i;
           break;
         }
 
-          if (j >= 0 && array[j] < pivot) {
-            int temp = array[j];
-            array[j] = array[i];
-            array[i] = temp;
-            tools::array_output(array, 7, "Swap");
-            break;
+        if (j >= 0 && array[j] < pivot) {
+          int temp = array[j];
+          array[j] = array[i];
+          array[i] = temp;
+//          tools::array_output(array, 14, "Swap");
+          break;
         }
       }
     }
   }
 
-  std::cout << "YOLO\n";
+  bool pivot_is_left = pivot_index == index_left;
+  bool pivot_is_right = pivot_index == index_right;
+
+  if (pivot_is_left) {
+//    std::cout << "LEFT\n";
+//    std::cout << pivot_index << std::endl;
+    quicksort(array, length, index_left + 1, index_right);
+  }
+  else if (pivot_is_right) {
+//    std::cout << "RIGHT\n";
+//    std::cout << pivot_index << std::endl;
+    quicksort(array, length, index_left, index_right - 1);
+  }
+  else {
+//    std::cout << "ELSE\n";
+//    std::cout << pivot_index << std::endl;
+
+    quicksort(array, length, index_left, pivot_index - 1);
+    quicksort(array, length, pivot_index + 1, index_right);
+  }
 }
-
-
-
-
-
-//void quicksort(int array[], int length) {
-//  int index_left = 0;
-//  int index_right = length -1;
-//  int pivot = array[index_right];
-//
-//  for (int j = index_right - 1; j > 0; j--) {
-//    if (array[j] < pivot) {
-//      for (int i = index_left; i < index_right - 1; i++) {
-//        if (j < i) {
-//          array[index_right] = array[i];
-//          array[i] = pivot;
-//          std::cout << "right before Quicksort_rekursiv" << std::endl;
-//          tools::array_output(array,7);
-////          int half = index_right/2;
-////          quicksort(array, index_left, half -1, false);
-////          quicksort(array, half, index_right, false);
-//          return;
-//        }
-//        if (array[i] >=  pivot) {
-//          int temp = array[j];
-//          array[j] = array[i];
-//          array[i] = temp;
-//          break;
-//        }
-//      }
-//    }
-//  }
-//}
-//
-//void quicksort(int array[], int index_left, int index_right, bool breaking) {
-//  int pivot = array[index_right];
-//  std::cout << "Quicksort_rekursiv start" << std::endl;
-//  tools::array_output(array,7);
-//  std::cout << "Pivot " << pivot << std::endl;
-//  std::cout << "Left " << index_left << std::endl;
-//  std::cout << "Right " << index_right << std::endl;
-//
-//
-//  for (int j = index_right - 1; j > 0; j--) {
-//    if (array[j] < pivot) {
-//      for (int i = index_left; i < index_right - 1; i++) {
-//        if (j <= i) {
-//          array[index_right] = array[i];
-//          array[i] = pivot;
-//          int half = index_right/2;
-//          std::cout << "half " << half << std::endl;
-//
-//          if (half > 3) {
-//                      if (breaking) return;
-//                      quicksort(array, index_left, half, false);
-//                      quicksort(array, half - 1, index_right, true);
-//          }
-//          return;
-//        }
-//        if (array[i] >= pivot) {
-//          int temp = array[j];
-//          array[j] = array[i];
-//          array[i] = temp;
-//          break;
-//        }
-//      }
-//    }
-//  }
-//  std::cout << "Quicksort_rekursiv end" << std::endl;
-//  tools::array_output(array,7);
-//}
