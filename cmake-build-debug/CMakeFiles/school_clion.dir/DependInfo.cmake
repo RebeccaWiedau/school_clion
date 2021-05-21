26b69d6c2a44012ab6cf4fcd9b454159069f8b6a
Rebecca Wiedau <rebecca.wiedau@nrwsoft.de>
2021-05-21 14:05:25 +0200

    Git ignore

---
+++ cmake-build-debug/CMakeFiles/school_clion.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/home_rebecca/projects/school_clion/main.cpp" "/home/home_rebecca/projects/school_clion/cmake-build-debug/CMakeFiles/school_clion.dir/main.cpp.o"
+  "/home/home_rebecca/projects/school_clion/sort/bubblesort.cpp" "/home/home_rebecca/projects/school_clion/cmake-build-debug/CMakeFiles/school_clion.dir/sort/bubblesort.cpp.o"
+  "/home/home_rebecca/projects/school_clion/sort/quicksort.cpp" "/home/home_rebecca/projects/school_clion/cmake-build-debug/CMakeFiles/school_clion.dir/sort/quicksort.cpp.o"
+  "/home/home_rebecca/projects/school_clion/student_array/student_array.cpp" "/home/home_rebecca/projects/school_clion/cmake-build-debug/CMakeFiles/school_clion.dir/student_array/student_array.cpp.o"
+  "/home/home_rebecca/projects/school_clion/tools/output.cpp" "/home/home_rebecca/projects/school_clion/cmake-build-debug/CMakeFiles/school_clion.dir/tools/output.cpp.o"
+  "/home/home_rebecca/projects/school_clion/tools/random_array.cpp" "/home/home_rebecca/projects/school_clion/cmake-build-debug/CMakeFiles/school_clion.dir/tools/random_array.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/school_clion.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/school_clion.dir/main.cpp.o"
+  "CMakeFiles/school_clion.dir/sort/bubblesort.cpp.o"
+  "CMakeFiles/school_clion.dir/sort/quicksort.cpp.o"
+  "CMakeFiles/school_clion.dir/student_array/student_array.cpp.o"
+  "CMakeFiles/school_clion.dir/tools/output.cpp.o"
+  "CMakeFiles/school_clion.dir/tools/random_array.cpp.o"
+  "school_clion"
+  "school_clion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/school_clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
